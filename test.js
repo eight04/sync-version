144867a44bb536c4300b23864f32b3f2a1ee7736
eight <eight04@gmail.com>
2016-11-07 10:27:50 +0800

    Add test script

---
+++ test.js
@@ -0,0 +1,17 @@
+var {execSync: exec} = require("child_process"),
+	assert = require("assert"),
+
+	samples = ["sample.json", "sample.js", "sample.user.js"],
+	pkg = require("./package.json");
+	
+samples.forEach(fn => {
+	fn = "test/" + fn;
+    assert.equal(
+        exec(
+            `node sync-version.js --list ${fn}`,
+            {encoding: "utf8"}
+        ),
+        `package.json\t${pkg.version}\n${fn}\t0.1.0\n`
+    );
+});
+	
