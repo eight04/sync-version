e668faa5c00ea7ca6ff19eec7c6fe430698f7e04
eight <eight04@gmail.com>
2017-05-16 03:03:22 +0800

    Release v1.0.1

--- sync-version.js
+++ sync-version.js
@@ -14,7 +14,7 @@ Options:
 	path = require("path"),
 	pkg = getPackage(),
 	
-	args = docopt.docopt(doc, {version: "1.0.0"}),
+	args = docopt.docopt(doc, {version: "1.0.1"}),
 	
 	res = [
 		/\bversion["']?:\s*["']?([^\s'"]+)/,
