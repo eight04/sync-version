ab1fe345046d1ea716e5acd3493de3c666da3e19
eight04 <eight04@gmail.com>
2025-09-17 23:54:58 +0800

    Fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,19 @@ import globals from "globals";
 
 export default [
   {
-    ignores: ["dist-extension/*", "build", "chrome"]
+    ignores: ["dist-extension/*", "build", "chrome", "test/*"]
   },
   js.configs.recommended,
   {
     "rules": {
       "dot-notation": 2,
       "max-statements-per-line": 2,
+      "no-unused-vars": [2, { "caughtErrors": "none" }],
     },
     languageOptions: {
       globals: {
         ...globals.browser,
-        // ...globals.webextensions,
+        ...globals.node,
       }
     },
   },
